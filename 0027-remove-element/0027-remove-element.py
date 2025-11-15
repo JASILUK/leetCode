9a9402ebbc1d0711084a05457ec7f6ed0ef008ed
JASIL <mohdjasil2004@gmail.com>
2025-11-15 09:40:09 +0530

    Time: 0 ms (100%), Space: 17.7 MB (83.06%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,8 +1,11 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        removearr = []
-        while val in nums:
-            nums.remove(val)
-            removearr.append('_')
-        result = sorted(nums).extend(removearr)
-        return result
+        k = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k  +=1
+
+        return k
+
+            
